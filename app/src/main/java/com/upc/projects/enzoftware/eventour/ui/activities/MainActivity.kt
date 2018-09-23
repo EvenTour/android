09bf0d8d832e69e358e868884d01931a0d71c55d
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-09-22 22:34:27 -0500

    Working navigation bottom

--- app/src/main/java/com/upc/projects/enzoftware/eventour/ui/activities/MainActivity.kt
+++ app/src/main/java/com/upc/projects/enzoftware/eventour/ui/activities/MainActivity.kt
@@ -2,28 +2,21 @@ package com.upc.projects.enzoftware.eventour.ui.activities
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.EventLog
+import android.view.MenuItem
 import com.upc.projects.enzoftware.eventour.R
+import com.upc.projects.enzoftware.eventour.ui.fragments.EventFragment
+import com.upc.projects.enzoftware.eventour.ui.fragments.FavoriteFragment
+import com.upc.projects.enzoftware.eventour.ui.fragments.ProfileFragment
+import com.upc.projects.enzoftware.eventour.ui.fragments.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+            return@OnNavigationItemSelectedListener navigateTo(item)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +24,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.selectedItemId = R.id.navigation_event
+    }
+
+    private fun fragmentFor(item: MenuItem):Fragment{
+        when(item.itemId){
+            R.id.navigation_event ->{
+                return EventFragment()
+            }
+            R.id.navigation_favorites ->{
+                return FavoriteFragment()
+            }
+            R.id.navigation_profile ->{
+                return ProfileFragment()
+            }
+            R.id.navigation_search -> {
+                return SearchFragment()
+            }
+        }
+        return EventFragment()
+    }
+
+
+    private fun navigateTo(item : MenuItem):Boolean{
+        item.isChecked = true
+        return supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.contentMain,fragmentFor(item))
+                .commit() > 0
     }
 }
