5d857241a2112d105839eddbeee73951dae9fa9f
RodrigoAGM <rod-gm@hotmail.com>
2018-10-25 01:43:33 -0500

    User profile updated

--- app/src/main/java/com/upc/projects/enzoftware/eventour/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/upc/projects/enzoftware/eventour/ui/fragments/ProfileFragment.kt
@@ -6,16 +6,26 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 import com.upc.projects.enzoftware.eventour.R
 
 class ProfileFragment : Fragment() {
 
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-    }
+        val view =  inflater.inflate(R.layout.fragment_profile, container, false)
 
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+        val nameView:TextView = view.findViewById(R.id.userProfileName)
+        nameView.text = user!!.displayName.toString()
+
+        return view
+    }
 
 }
